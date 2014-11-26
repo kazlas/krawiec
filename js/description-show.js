2a45c9111815fa2cd2f0e673dd4bd0be90bc6e33
kazlas <kazlas@users.noreply.github.com>
2014-11-26 10:07:16 +0100

    Dimm curtain does not affect text opacity

--- js/description-show.js
+++ js/description-show.js
@@ -10,34 +10,45 @@
 $(document).ready(function(){
   	
   $("ul#divMenu").click(function() {
-	  animateDescription ("div#divCurtain");
+	  animateCurtain ("div#divCurtain");
+	  animateDescription ("div#divDescription");
   });
 
+  function animateCurtain (element) {
+	$(element).finish();  
+	animateLeftAndDimm (element);
+	animateRightAndGlow (element);
+  }
 
   function animateDescription (element) {
-	   
 	$(element).finish();  
 	clearDescription(element);
 	animateLeft (element);
 	animateRight (element);
-
+	
 	setTimeout(function () {
 	    showDescription (element, currentMenuDescriptionId, "pl")}
 	    , 1000);
   }
 
-  function animateRight (element) {
-    $(element).animate({left:"+=170px",opacity:"0.5"},"slow");
+  function animateRightAndGlow (element) {
+    $(element).animate({left:"+=170px", opacity: 0.5},"slow");
+  }
 
+  function animateLeftAndDimm (element) {
+    $(element).animate({left:"-=170px", opacity: 0.1},"slow");
+  }
+
+  function animateRight (element) {
+    $(element).animate({left:"+=170px"},"slow");
   }
 
   function animateLeft (element) {
-    $(element).animate({left:"-=170px",opacity:"0.1"},"slow");
-
+    $(element).animate({left:"-=170px"},"slow");
   }
 
   function clearDescription (displayElement) {
-	  $(displayElement).text("");
+	  $(displayElement).html("");
   }
 
   function showDescription (displayElement, descriptionId, lang) {
@@ -61,7 +72,6 @@ $(document).ready(function(){
 	  }
 
 	  html += ">";
-
 	  return html;
   }
 
